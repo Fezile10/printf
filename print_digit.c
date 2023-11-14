66c0ee46a62ef35f1a72187de17f7480bf9df337
Obrabigfish <Obrabigfish1@gmail.com>
2023-11-14 12:03:53 -0800

    second

---
+++ print_digit.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+
+/**
+ * print_digit - print integers.
+ * @args: argument.
+ * Return: count of chars.
+ */
+
+int print_digit(va_list args)
+{
+	int decimal = 1;
+	int count = 0;
+	long int digit = va_arg(args, int);//negative 
+	long int digitaux;
+
+	if (digit < 0)
+	{
+		count += _putchar('-');
+		digit *= -1;
+	}
+
+	if (digit < 10)
+		return (count += _putchar(digit + '0'));
+
+	digitaux = digit;
+
+	while (digitaux > 9)
+	{
+		decimal *= 10;
+		digitaux /= 10;
+	}
+	while (decimal >= 1)
+	{
+		count += _putchar(((digit / decimal) % 10) + '0');
+		decimal /= 10;
+	}
+
+	return (count);
+}
---
+++ print_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_string - print string.
+ * @args: argument.
+ * Return: character count.
+ */
+
+int print_string(va_list args)
+{
+	int i;
+	int count= 0;
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(null)";
+
+	if (str[0] == '\0')
+		return (-1);
+
+	for (i = 0; str[i] != '\0'; i++)//
+		count += _putchar(str[i]);
+
+	return (count);
+}
